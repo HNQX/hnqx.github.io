14a519b9a8a22a2e28b86bc2154acd1a4b8ec688
han <hannxq@qq.com>
2024-08-09 10:19:55 +0800

    Update basemap.js

--- sub/shp/basemap.js
+++ sub/shp/basemap.js
@@ -1,7 +1,7 @@
 window.mapboxParams = {
   js: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.js',
   css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
-  key: 'pk.eyJ1IjoiZ3JhbW1hdGEiLCJhIjoiY2wxMzI4b3dqMDNhMjNpcDhzcmU0aGh5diJ9.m1lacx48pTYls-X3FVhG9g',
+  key: 'pk.eyJ1IjoiaGFuY2NjIiwiYSI6ImNsMXhibzY5bTAxYXEzY21tZHU2ODc2bXIifQ.wGIOMgOdYCeJA0nCz51PVQ',
   styles: [{
     name: 'Map',
     icon: 'images/thumb-map.jpg',
