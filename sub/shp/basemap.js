2b39c17a33e51ec706f0f6ebceba1c96fad4bdc8
han <hannxq@qq.com>
2024-08-09 10:45:40 +0800

    Update basemap.js

--- sub/shp/basemap.js
+++ sub/shp/basemap.js
@@ -3,13 +3,13 @@ window.mapboxParams = {
   css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
   key: 'pk.eyJ1IjoiaGFuY2NjIiwiYSI6ImNsem0za2RscTBhZzYyeW9wZTB0N3hjcTUifQ.tn61FaIE2sgmyr5QppRW3w',
   styles: [{
-    name: 'Map',
+    name: 'Streets',
     icon: 'images/thumb-map.jpg',
-    url: 'mapbox://styles/grammata/cl0ymri0f000214lc1mgtweqk'
+    url: 'mapbox://styles/hanccc/clzm3tpry003u01pz9c4ab71q'
   }, {
     name: 'Satellite',
     icon: 'images/thumb-satellite.jpg',
     dark: true,
-    url: 'mapbox://styles/grammata/cl0ymvkkl004115mlgf5o442n'
+    url: 'mapbox://styles/hanccc/clzm3sva1003m01r833og67x9'
   }]
 };
