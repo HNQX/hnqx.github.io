7a6af5438ee5667611f89a2a60db7a9b47bbd205
han <hannxq@qq.com>
2024-08-09 11:00:43 +0800

    e

--- sub/shp/basemap.js
+++ sub/shp/basemap.js
@@ -2,14 +2,38 @@ window.mapboxParams = {
   js: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.js',
   css: 'https://api.mapbox.com/mapbox-gl-js/v2.11.1/mapbox-gl.css',
   key: 'pk.eyJ1IjoiaGFuY2NjIiwiYSI6ImNsem0za2RscTBhZzYyeW9wZTB0N3hjcTUifQ.tn61FaIE2sgmyr5QppRW3w',
-  styles: [{
-    name: 'Streets',
-    icon: 'images/thumb-map.jpg',
-    url: 'mapbox://styles/mapbox/standard'
-  }, {
-    name: 'Satellite',
-    icon: 'images/thumb-satellite.jpg',
-    dark: true,
-    url: 'mapbox://styles/mapbox/standard-satellite'
-  }]
+  styles: [
+    {
+      name: 'Streets',
+      icon: 'images/thumb-map.jpg',
+      url: 'mapbox://styles/mapbox/standard'
+    },
+    {
+      name: 'Satellite',
+      icon: 'images/thumb-satellite.jpg',
+      dark: true,
+      url: 'mapbox://styles/mapbox/standard-satellite'
+    },
+    {
+      name: 'Streets',
+      icon: 'images/thumb-1.png',
+      url: 'mapbox://styles/mapbox/streets-v12'
+    },
+    {
+      name: 'Outdoors',
+      icon: 'thumb-2.png',
+      url: 'mapbox://styles/mapbox/outdoors-v12'
+    },
+    {
+      name: 'Light',
+      icon: 'thumb-3.png',
+      url: 'mapbox://styles/mapbox/light-v11'
+    },
+    {
+      name: 'Dark',
+      icon: 'images/thumb-4.png',
+      dark: true,
+      url: 'mapbox://styles/mapbox/dark-v11'
+    }
+  ]
 };
